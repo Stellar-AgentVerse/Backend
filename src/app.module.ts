34d5d6b811a58c4efdc4ef0f754708a8ce7d731a
Joaco2603 <jpappa2603@gmail.com>
2026-06-15 00:27:32 -0600

    feat: register Assets, Wallet, Marketplace, Dashboard modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,10 @@ import { sorobanConfig } from './tokens/config/soroban.config';
 import { jwtConfig } from './config/jwt.config';
 import { DatabaseModule } from './database/database.module';
 import { IndexerModule } from './indexer/indexer.module';
+import { AssetsModule } from './assets/assets.module';
+import { WalletModule } from './wallet/wallet.module';
+import { MarketplaceModule } from './marketplace/marketplace.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -19,6 +23,10 @@ import { IndexerModule } from './indexer/indexer.module';
     TokensModule,
     AuthModule,
     IndexerModule,
+    AssetsModule,
+    WalletModule,
+    MarketplaceModule,
+    DashboardModule,
   ],
   controllers: [],
   providers: [],
